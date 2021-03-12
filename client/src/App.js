ae99ac0e0b27207ec3fe95c9fff185a5173c072e
James Simon <notminebydesign@gmail.com>
2021-03-12 11:22:49 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 function App() {
   return (
     <div>
+      {/* insert wrappers inside of Auth0Provider */}
+      <Auth0Provider
+
+    domain= {process.env.DOMAIN}
+    clientId={process.env.CLIENT_ID}
+    redirectUri={window.location.origin}
+  >
+          <Navbar/>
+  </Auth0Provider>
       <Navbar/>
     </div>
   );
 }
 
 export default App;
+
