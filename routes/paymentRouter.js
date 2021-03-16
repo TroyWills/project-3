e399e084d8499e95dc486174d81e8cb0f5c2d6e4
inqueblot <notminebydesign@gmail.com>
2021-03-16 02:10:08 -0500

    bowie

---
+++ routes/paymentRouter.js
@@ -0,0 +1,20 @@
+const stripe = require('stripe')(process.env.SECRET_TEST_APIKEY);
+
+async function paymentRouter(req, res) {
+    console.log(req.body.token)
+    try {
+        console.log('route hit')
+        let { status } = await stripe.charges.create({
+            amount: req.body.amount,
+            currency: 'usd',
+            source: req.body.token,
+        })
+        return res.json({ status })
+    } catch (err) {
+        console.error(err)
+        console.log('router not hit')
+        res.status(500).end()
+    }
+}
+
+module.exports = paymentRouter
