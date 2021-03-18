376c62196a733c8712a9445ffeb3d413072a47c1
inqueblot <notminebydesign@gmail.com>
2021-03-18 11:44:58 -0500

    add image requirement, update grocery model

--- models/grocery.js
+++ models/grocery.js
@@ -4,8 +4,12 @@ const Schema = mongoose.Schema;
 const grocerySchema = new Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
+    description: { type: String, required: false },
     category: { type: String, required: true },
-    quantity: { type: Number, required: true }
+    quantity: { type: Number, required: true },
+    img: { type: String, required: true },
+    width: { type: String, required: false }
+
 });
 
 const Grocery = mongoose.model("Grocery", grocerySchema);
