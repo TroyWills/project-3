ca20259dec8f520d8ab5bbaa3f364c3f5e1d3e21
admin <admin@elizabeth.local>
2021-03-18 10:12:25 -0500

    fade

--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -1,16 +1,19 @@
 // import { useEffect, useState } from "react";
 import ItemCard from "../../components/Items/ItemCard"
 import "./style.css";
+import Fade from "react-reveal/Fade";
 
 const Home = (props) => {
 
   return (
+    
     <div className="body">
       <div className="heading1">The Mini Mart</div>
       <div className="items">
         {props.filteredProducts &&
           props.filteredProducts.map((product) => {
             return (
+              <Fade top>
               <ItemCard
                 key={product.name}
                 img={product.img}
@@ -20,6 +23,7 @@ const Home = (props) => {
                 width={product.width}
               // height={product.height}
               />
+              </Fade>
             );
           })}
       </div>
